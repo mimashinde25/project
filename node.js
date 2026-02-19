6f064e80e4965db1c18dfd433b7e906011ab378c
mimashinde25 <130497355+mimashinde25@users.noreply.github.com>
2026-02-19 07:49:40 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('🚀 Node.js App Deployed Successfully via Jenkins!');
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'UP', timestamp: new Date() });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
